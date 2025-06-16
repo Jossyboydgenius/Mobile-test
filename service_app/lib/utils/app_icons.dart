944917b41d5bcff03c652b9e7e49190fce4a3989
jossyboydgenius <jossyboydgenius@gmail.com>
2025-06-16 12:07:44 +0000

    Add AppIcons utility class for managing icon paths

---
+++ service_app/lib/utils/app_icons.dart
@@ -0,0 +1,18 @@
+class AppIcons {
+  // Define paths to your icons here
+  static const String helpIcon = 'assets/icons/help_icon.png';
+  static const String notificationIcon = 'assets/icons/notification_icon.png';
+  static const String profileIcon = 'assets/icons/profile_icon.png';
+  static const String washingMachineIcon =
+      'assets/icons/washing_machine_icon.png';
+  static const String plumbingIcon = 'assets/icons/plumbing_icon.png';
+  static const String cleaningIcon = 'assets/icons/cleaning_icon.png';
+  static const String hvacIcon = 'assets/icons/hvac_icon.png';
+  static const String homeIcon = 'assets/icons/home_icon.png';
+  static const String bookingIcon = 'assets/icons/booking_icon.png';
+  static const String messageIcon = 'assets/icons/message_icon.png';
+  static const String accountIcon = 'assets/icons/account_icon.png';
+  static const String verifiedIcon = 'assets/icons/verified_icon.png';
+  static const String starIcon = 'assets/icons/star_icon.png';
+  static const String medalIcon = 'assets/icons/medal_icon.png';
+}
