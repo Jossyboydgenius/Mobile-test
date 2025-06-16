baea4ddea9bf695120fcc4672e291138c1b358de
jossyboydgenius <jossyboydgenius@gmail.com>
2025-06-16 12:07:55 +0000

    Add AppImages utility class for managing image paths

---
+++ service_app/lib/utils/app_images.dart
@@ -0,0 +1,7 @@
+class AppImages {
+  // Define paths to your images here
+  static const String profileImage = 'assets/images/profile_image.png';
+  static const String worker1 = 'assets/images/worker1.png';
+  static const String worker2 = 'assets/images/worker2.png';
+  static const String worker3 = 'assets/images/worker3.png';
+}
