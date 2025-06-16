df28243bf848c8513871cebdb4c9bb1727f47b44
jossyboydgenius <jossyboydgenius@gmail.com>
2025-06-16 12:07:37 +0000

    Add AppColors utility class for consistent color management

---
+++ service_app/lib/utils/app_colors.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  // Main colors
+  static const Color primaryColor = Color(0xFF9C28B1);
+  static const Color redColor = Color(0xFFBF0303);
+
+  // Background color
+  static const Color backgroundColor = Color(0xFFFCF6FD);
+
+  // Common colors
+  static const Color white = Colors.white;
+  static const Color black = Colors.black;
+  static const Color grey = Color(0xFF8F8F8F);
+  static const Color lightGrey = Color(0xFFE5E5E5);
+
+  // Status colors
+  static const Color successColor = Color(0xFF4CAF50);
+  static const Color warningColor = Color(0xFFFFC107);
+  static const Color errorColor = redColor;
+}
