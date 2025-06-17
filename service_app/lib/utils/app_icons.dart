7e19cf6a67d6ea5655d531e8164361dacfef35bc
jossyboydgenius <jossyboydgenius@gmail.com>
2025-06-17 13:39:53 +0000

    Refactor AppIcons to replace PNG icons with SVG icons for improved scalability and consistency

--- service_app/lib/utils/app_icons.dart
+++ service_app/lib/utils/app_icons.dart
@@ -1,18 +1,25 @@
 class AppIcons {
   // Define paths to your icons here
-  static const String helpIcon = 'assets/icons/help_icon.png';
-  static const String notificationIcon = 'assets/icons/notification_icon.png';
-  static const String profileIcon = 'assets/icons/profile_icon.png';
-  static const String washingMachineIcon =
-      'assets/icons/washing_machine_icon.png';
-  static const String plumbingIcon = 'assets/icons/plumbing_icon.png';
-  static const String cleaningIcon = 'assets/icons/cleaning_icon.png';
-  static const String hvacIcon = 'assets/icons/hvac_icon.png';
-  static const String homeIcon = 'assets/icons/home_icon.png';
-  static const String bookingIcon = 'assets/icons/booking_icon.png';
-  static const String messageIcon = 'assets/icons/message_icon.png';
-  static const String accountIcon = 'assets/icons/account_icon.png';
-  static const String verifiedIcon = 'assets/icons/verified_icon.png';
-  static const String starIcon = 'assets/icons/star_icon.png';
-  static const String medalIcon = 'assets/icons/medal_icon.png';
+  // SVG icons
+  static const String bookingsIcon = 'assets/icons/bookings.svg';
+  static const String checkIcon = 'assets/icons/check.svg';
+  static const String customerSupportIcon = 'assets/icons/customer-support.svg';
+  static const String customerHelpIcon = 'assets/icons/customer-help.svg';
+  static const String homeIcon = 'assets/icons/home.svg';
+  static const String messageIcon = 'assets/icons/message.svg';
+  static const String notificationIcon = 'assets/icons/notification.svg';
+  static const String profileIcon = 'assets/icons/profile.svg';
+
+  // Regular icons
+  static const String washingMachineIcon = 'assets/images/laundry.png';
+  static const String plumbingIcon = 'assets/images/plumbing.png';
+  static const String cleaningIcon = 'assets/images/cleaning.png';
+  static const String hvacIcon = 'assets/images/hvac.png';
+  static const String userIcon = 'assets/images/user.png';
+
+  // For Material icons fallback
+  static const String verifiedIcon = 'verified';
+  static const String starIcon = 'star';
+  static const String medalIcon = 'workspace_premium';
+  static const String helpIcon = 'headset_mic';
 }
