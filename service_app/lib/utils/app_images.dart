95453a618beccfe12c0039311d08f3b274ff3321
jossyboydgenius <jossyboydgenius@gmail.com>
2025-06-17 13:39:59 +0000

    Update image paths in AppImages for consistency and clarity

--- service_app/lib/utils/app_images.dart
+++ service_app/lib/utils/app_images.dart
@@ -1,7 +1,7 @@
 class AppImages {
   // Define paths to your images here
-  static const String profileImage = 'assets/images/profile_image.png';
-  static const String worker1 = 'assets/images/worker1.png';
-  static const String worker2 = 'assets/images/worker2.png';
-  static const String worker3 = 'assets/images/worker3.png';
+  static const String profileImage = 'assets/images/user.png';
+  static const String worker1 = 'assets/images/image.png';
+  static const String worker2 = 'assets/images/image1.png';
+  static const String worker3 = 'assets/images/image2.png';
 }
