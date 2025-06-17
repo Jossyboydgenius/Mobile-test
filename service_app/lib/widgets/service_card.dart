440b547e6d651b86e8f77a92bad70ef24c972ea5
jossyboydgenius <jossyboydgenius@gmail.com>
2025-06-17 13:40:25 +0000

    Refactor ServiceCard to use asset images directly, simplifying icon handling and improving code clarity

--- service_app/lib/widgets/service_card.dart
+++ service_app/lib/widgets/service_card.dart
@@ -12,52 +12,43 @@ class ServiceCard extends StatelessWidget {
     required this.title,
   });
 
-  IconData _getIconData() {
-    switch (icon) {
-      case 'local_laundry_service':
-        return Icons.local_laundry_service;
-      case 'plumbing':
-        return Icons.plumbing;
-      case 'cleaning_services':
-        return Icons.cleaning_services;
-      case 'hvac':
-        return Icons.hvac;
-      default:
-        return Icons.home_repair_service;
-    }
-  }
+  // Use the icon path directly from the parameter
 
   @override
   Widget build(BuildContext context) {
-    return SizedBox(
-      width: 80,
+    return Container(
+      width: 110, // Fixed width for all cards
+      height: 130, // Fixed height for all cards
+      padding: const EdgeInsets.all(10),
+      decoration: BoxDecoration(
+        color: AppColors.white,
+        borderRadius: BorderRadius.circular(12),
+        border: Border.all(color: AppColors.borderGrey),
+        boxShadow: [
+          BoxShadow(
+            color: Colors.grey.withOpacity(0.1),
+            spreadRadius: 1,
+            blurRadius: 4,
+            offset: const Offset(0, 2),
+          ),
+        ],
+      ),
       child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
         children: [
-          Container(
-            padding: const EdgeInsets.all(10),
-            decoration: BoxDecoration(
-              color: AppColors.white,
-              borderRadius: BorderRadius.circular(12),
-              boxShadow: [
-                BoxShadow(
-                  color: Colors.grey.withOpacity(0.1),
-                  spreadRadius: 1,
-                  blurRadius: 4,
-                  offset: const Offset(0, 2),
-                ),
-              ],
-            ),
-            child: Icon(
-              _getIconData(),
-              size: 40,
-              color: AppColors.primaryColor,
-            ),
+          Image.asset(
+            icon,
+            width: 50,
+            height: 50,
           ),
           const SizedBox(height: 8),
           Text(
             title,
             textAlign: TextAlign.center,
-            style: AppTextStyle.smallText,
+            style: AppTextStyle.latoRegular(
+              size: 11, // Reduced text size
+              weight: FontWeight.w500,
+            ),
             maxLines: 2,
             overflow: TextOverflow.ellipsis,
           ),
